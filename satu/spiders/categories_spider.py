1dd0b63ecc02a9dcc86396ede52cda097f3de857
Narryss <144238002+Narryss@users.noreply.github.com>
2025-05-26 02:38:35 +0300

    Update categories_spider.py

--- satu/spiders/categories_spider.py
+++ satu/spiders/categories_spider.py
@@ -1,4 +1,7 @@
 import scrapy
+import re
+import json
+from satu.items import CategoriesItem
 
 class CategoriesSpiderSpider(scrapy.Spider):
     name = "categories_spider"
@@ -6,9 +9,33 @@ class CategoriesSpiderSpider(scrapy.Spider):
     start_urls = ["https://satu.kz"]
 
     def parse(self, response):
-        categories = response.xpath('//ul[@class="nujFR"]/li[@class="YSmsd"]')
-        for category in categories:
-            yield {
-                'category_name': category.xpath('.//span[@class="_3Trjq"]//text()').get().strip(),
-                'category_url': response.urljoin(category.xpath('./a[@class="_0cNvO JcwiH"]/@href').get())
-            }
+        item = CategoriesItem()
+
+        script_data = response.xpath("//script[contains(text(), 'window.ApolloCacheState =')]/text()").get()
+
+        raw_data = re.search(
+            pattern=r'window\._NEW_CATALOG_UI_SSR\s*=\s*.*?window\.ApolloCacheState\s*=\s*({.*?});',
+            string=script_data,
+            flags=re.DOTALL
+        )
+
+        apollo_data = json.loads(raw_data.group(1))
+
+        root_query = apollo_data.get('ROOT_QUERY', {})
+
+        megamenu_data = None
+        for key in root_query:
+            if key.startswith('megamenu'):
+                megamenu_data = root_query[key]
+                break
+
+        if megamenu_data:
+            categories = []
+            for cat in megamenu_data['categories']:
+                category_url = f"https://satu.kz/{cat.get('alias')}"
+                categories.append({
+                    'url': category_url
+                })
+            item['categories'] = categories
+
+        return item
