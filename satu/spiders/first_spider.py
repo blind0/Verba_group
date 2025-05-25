aa31362071e5fedb83b9267ba80fc67c0df80e04
Narryss <144238002+Narryss@users.noreply.github.com>
2025-05-26 02:40:15 +0300

    Update first_spider.py

--- satu/spiders/first_spider.py
+++ satu/spiders/first_spider.py
@@ -1,3 +1,4 @@
+import math
 import scrapy
 import json
 import re
@@ -9,104 +10,33 @@ class SatuSpider(scrapy.Spider):
     allowed_domains = ["satu.kz"]
     start_urls = ["https://satu.kz"]
 
-    CATEGORIES = {
-        "electronics": "Tehnika-i-elektronika",
-        "construction": "Stroitelstvo",
-        "home": "Dom-i-sad",
-        "repair": "Materialy-dlya-remonta",
-        "jewelry": "Ukrasheniya-i-chasy",
-        "beauty": "Krasota-i-zdorove",
-        "clothes": "Odezhda",
-        "food": "Produkty-pitaniya-napitki",
-        "medicine": "Medikamenty-meditsinskie-tovary",
-        "kids": "Tovary-dlya-detej",
-        "tools": "Instrument"
-    }
-
     def __init__(self, category=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.category = category
+        self.categories = {}
+
+        with open('categories.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            for item in data:
+                categories_list = item.get('categories', [])
+                for cat in categories_list:
+                    full_url = cat.get('url', {}).strip()
+                    if full_url:
+                        url = full_url.split('/')[-1]
+                        self.categories[url] = None
 
     def parse(self, response):
-        if self.category:
-            if self.category == 'all':
-                for cat_url in self.CATEGORIES.values():
-                    yield response.follow(f"/{cat_url}", self.parse_category)
-            elif self.category in self.CATEGORIES:
-                yield response.follow(f"/{self.CATEGORIES[self.category]}", self.parse_category)
-        else:
-            yield response.follow("/Tehnika-i-elektronika", self.parse_category)
+        if self.category == 'all':
+            for cat_url in self.categories:
+                yield response.follow(f"https://satu.kz/{cat_url}", self.parse_category)
+        elif self.category in self.categories:
+            yield response.follow(f"https://satu.kz/{self.category}", self.parse_category)
 
     def parse_category(self, response):
         product_links = response.xpath('//div[@data-qaid="product_gallery"]'
-                                       '//a[@data-qaid="product_link"]/@href').getall()
+                                       '//a[@data-qaid and not(@data-qaid="seo_carousel")]/@href').getall()
         for link in product_links:
             yield response.follow(link, self.parse_item)
-
-        next_page = response.xpath('//div[@data-qaid="pagination"]//a[@data-qaid="next_page"]/@href').get()
-        if next_page:
-            yield response.follow(next_page, self.parse_category)
-
-    def parse_item(self, response):
-        item = SatuItem()
-
-        MafxA_item = response.xpath('.//div[@class="MafxA _6xArK"]/div[@class="l-GwW"]')
-        tqUsL_item = response.xpath('.//div[@class="tqUsL"]')
-
-        item['url'] = response.url
-
-        name_text = response.xpath('.//div[@class="l-GwW fvQVX"]/h1[@data-qaid="product_name"]/text()').get()
-        item["product_name"] = ' '.join(name_text.strip().split()) if name_text else None
-
-        item["img"] = response.xpath('.//div[@data-qaid="image_block"]//img/@src').getall()
-
-        item["availability"] = MafxA_item.xpath('.//span[@data-qaid="product_presence"]/text()').get()
-
-        discounted_price = tqUsL_item.xpath('.//div[@class="IP36L bkjEo"]/span[@class="yzKb6"]/text()').get()
-
-        if discounted_price:
-            item["current_price"] = discounted_price.strip()
-        else:
-            regular_price = tqUsL_item.xpath('.//div[@class="bkjEo"]/span[@class="yzKb6"]/text()').get()
-            item["current_price"] = regular_price.strip() if regular_price else None
-
-        old_price = tqUsL_item.xpath('.//span[@class="XXdUM bkjEo"]/span[@class="yzKb6"]/text()').get()
-        item["old_price"] = old_price.strip() if old_price else None
-
-        item["product_count"] = response.xpath('.//div[@class="MafxA _6xArK"]//div[@class="l-GwW"]'
-                                               '/span[@data-qaid="order_counter"]/text()').get()
-
-        rating = response.xpath('.//div[@class="MafxA iU70- WIR6H ZNZm3"]//span[@class="_3Trjq snf--"]/text()').get()
-        item["product_rating"] = float(rating) if rating else None
-
-        reviews_count = response.xpath('.//div[@class="MafxA _0NLAD"]//span[@data-qaid="opinion_count"]/text()').get()
-        item["reviews_count"] = int(reviews_count) if reviews_count else None
-
-        reviews = []
-        for review in response.xpath('.//li[@class="bfetG"]'):
-            author = review.xpath('.//span[@data-qaid="author_name"]/text()').get()
-            date = review.xpath('.//span[@data-qaid="date_created"]/text()').get()
-            text = review.xpath('.//span[@data-qaid="title"]/text()').get()
-
-            if any([author, date, text]):
-                reviews.append({
-                    'author': author,
-                    'date': date,
-                    'text': text
-                })
-
-        item['reviews'] = reviews if reviews else None
-
-        sellers = []
-        for seller in response.xpath('.//div[@class="M3v0L qzGRQ -Zy0Z"]'):
-            sellers.append({
-                'seller_name': seller.xpath('.//a[@data-qaid="company_name"]/text()').get(),
-                'link': response.urljoin(seller.xpath('.//a/@href').get()),
-                'time': seller.xpath('.//span[@data-qaid="company_age"]/text()').get(),
-                'orders_count': seller.xpath('.//span[@data-qaid="delivered_orders"]/text()').get(),
-                'seller_rating': seller.xpath('.//span[@class="_0cNvO jwtUM OX5sJ XCtBJ"]/text()').get()
-            })
-        item['sellers'] = sellers if sellers else None
 
         script_data = response.xpath("//script[contains(text(), 'window.ApolloCacheState =')]/text()").get()
 
@@ -116,39 +46,81 @@ class SatuSpider(scrapy.Spider):
             flags=re.DOTALL
         )
 
-        if raw_data:
-            apollo_data = json.loads(raw_data.group(1))
+        apollo_data = json.loads(raw_data.group(1))
 
-            root_query = apollo_data.get('ROOT_QUERY', {})
+        fast_cache = apollo_data.get('_FAST_CACHE', {})
 
-            product_card_data = None
-            for key in root_query:
-                if key.startswith('product'):
-                    product_card_data = root_query[key]
-                    break
+        category_data = None
+        for key in fast_cache:
+            if key.startswith('CategoryListingQuery'):
+                category_data = fast_cache[key]
+                break
 
-            if product_card_data:
-                description = product_card_data.get('descriptionPlain')
-                item['description'] = description
+        if category_data:
+            total_pages = category_data.get('result', {}).get('listing', {}).get('page', {}).get('total', {})
+            pages = math.ceil(total_pages / 48)
+            base_url = response.url.split(";")[0]
+            for page_num in range(2, pages + 1):
+                paginated_url = f"{base_url};{page_num}"
+                yield scrapy.follow(paginated_url, self.parse_category)
 
-                attributes = product_card_data.get('attributes', [])
-                parsed_attributes = []
+    def parse_item(self, response):
+        item = SatuItem()
 
-                for attr in attributes:
-                    name = attr.get('name')
-                    values = attr.get('values', [])
+        item['url'] = response.url
 
-                    for value_obj in values:
-                        value = value_obj.get('value')
-                        if name and value:
-                            parsed_attributes.append({
-                                'name': name,
-                                'value': value
-                            })
+        name_text = response.xpath('.//div[@class="l-GwW fvQVX"]/h1[@data-qaid="product_name"]/text()').get()
+        item['product_name'] = ' '.join(name_text.strip().split()) if name_text else None
 
-                item['attributes'] = parsed_attributes if parsed_attributes else None
-            else:
-                item['description'] = None
-                item['attributes'] = None
+        item['images'] = response.xpath('.//div[@data-qaid="image_block"]//img/@src').getall()
+
+        script_data = response.xpath("//script[contains(text(), 'window.ApolloCacheState =')]/text()").get()
+
+        json_match = re.search(
+            r'window\.ApolloCacheState\s*=\s*({.*?});',
+            script_data,
+            re.DOTALL
+        )
+
+        data = json.loads(json_match.group(1))
+        root_query = data.get('ROOT_QUERY', {})
+
+        fast_data = None
+        for key in root_query:
+            if key.startswith('product'):
+                fast_data = root_query[key]
+                break
+
+        item['description'] = fast_data.get('descriptionPlain')
+        item['availability'] = fast_data.get('presence', {}).get('isAvailable')
+        item['discount_price'] = fast_data.get('discountedPrice')
+        item['original_price'] = fast_data.get('priceOriginal')
+        item['product_count'] = fast_data.get('ordersCount')
+
+        opinion_counters = fast_data.get('productOpinionCounters', {})
+        item['product_rating'] = opinion_counters.get('rating')
+        item['reviews_count'] = opinion_counters.get('count')
+
+        company = fast_data.get('company', {})
+        if company:
+            item['company'] = [{
+                'name': company.get('name'),
+                'url': f"c{company.get('id', 'None')}-{company.get('slug', 'None')}.html",
+                'timeOn': company.get('ageYears'),
+                'orders': company.get('deliveredOrdersText'),
+                'rating': company.get('opinionStats', {}).get('opinionPositivePercent'),
+            }]
+
+        attributes = []
+        for attr in fast_data.get('attributes', []):
+            name = attr.get('name')
+            values = attr.get('values', [])
+            for val in values:
+                if val.get('value'):
+                    attributes.append({
+                        'name': name,
+                        'value': val.get('value')
+                    })
+        item['attributes'] = attributes if attributes else None
 
         return item
