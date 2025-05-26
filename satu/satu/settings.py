461ff46a21fa9e7ce5dc8823680bf369e68cd7c3
love_cube <ahapka1@gmail.com>
2025-05-26 23:41:16 +0500

    для satu.kz нужен был uagent

--- satu/satu/settings.py
+++ satu/satu/settings.py
@@ -13,7 +13,7 @@ SPIDER_MODULES = ["satu.spiders"]
 NEWSPIDER_MODULE = "satu.spiders"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "satu (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
